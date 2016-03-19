e6d780982869579855471c6eab43233017050ad0
Andrew Kovanda <kovanda@bizo.com>
2016-03-19 09:05:46 -0700

    moving queueitem to the module

--- lib/QueueItem.rb
+++ lib/QueueItem.rb
@@ -1,14 +1,16 @@
 require "digest"
-class QueueItem
-  attr_reader :id, :item
-  def initialize(item, name= "Item")
-    @id = Digest::SHA256.digest(rand(10**100).to_s + Time.now.to_i.to_s)
-    @item = item
-    @name = name
-  end
+module Thimble
+  class QueueItem
+    attr_reader :id, :item
+    def initialize(item, name= "Item")
+      @id = Digest::SHA256.digest(rand(10**100).to_s + Time.now.to_i.to_s)
+      @item = item
+      @name = name
+    end
 
-  def to_s
-    "#{@name}: #{@item} \nID: #{@id}"
-  end
+    def to_s
+      "#{@name}: #{@item} \nID: #{@id}"
+    end
 
+  end
 end
